e3de7f9a16f6435d7a36202fb899446c731decaa
A8D00 <125094247+A8D00@users.noreply.github.com>
2026-01-15 16:25:09 +0200

    Update codegen.py

--- compiler/codegen.py
+++ compiler/codegen.py
@@ -2,24 +2,16 @@
 # تحويل IR إلى Machine Code (0/1)
 
 def generate_machine_code(ir_functions):
+    """
+    ir_functions: قائمة IRFunction
+    return: string من 0 و 1
+    """
     binary = ""
 
     for fn in ir_functions:
         for instr in fn.body:
-
-            if instr.__class__.__name__ == "IRLoadConst":
-                binary += "00000001" + format(instr.value, "08b")
-
-            elif instr.__class__.__name__ == "IRAdd":
-                binary += "00000010" + "00000000"
-
-            elif instr.__class__.__name__ == "IRSub":
-                binary += "00000011" + "00000000"
-
-            elif instr.__class__.__name__ == "IRReturn":
-                binary += "11111111" + format(instr.value, "08b")
-
-            else:
-                raise RuntimeError("تعليمة IR غير مدعومة")
+            opcode_bits = format(instr.opcode, "08b")
+            binary += opcode_bits + instr.operand
 
     return binary
+    
