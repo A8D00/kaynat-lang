890f3a8cbeb031eaed4e5f4113425882c68cbe1f
A8D00 <125094247+A8D00@users.noreply.github.com>
2026-01-15 16:49:14 +0200

    Delete compiler directory

--- compiler/linker.py
+++
@@ -1,7 +0,0 @@
-# compiler/linker.py
-import subprocess
-import os
-
-def link(obj_file: str, output: str = "a.out"):
-    subprocess.run(["ld", obj_file, "-o", output], check=True)
-    return output
--- compiler/targets/arm64_linux.py
+++
@@ -1,5 +0,0 @@
-# target ARM64 Linux
-# سيتم التنفيذ لاحقًا
-
-def generate_exit(value: int) -> str:
-    raise NotImplementedError("ARM64 غير مدعوم بعد")
