b9921d7b3b4d68f247fc46eb37209c13ecbb3649
A8D00 <125094247+A8D00@users.noreply.github.com>
2026-01-16 03:32:42 +0200

    Delete runtime directory

--- runtime/run.py
+++
@@ -1,8 +0,0 @@
-from existence.state import State
-from existence.pattern import Pattern
-from existence.loop import loop
-
-def run():
-    state = State()
-    pattern = Pattern()
-    loop(state, pattern)
