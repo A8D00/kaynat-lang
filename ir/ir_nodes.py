4f3af765662ba0e2ff80611ebe96ddd8c58399ea
A8D00 <125094247+A8D00@users.noreply.github.com>
2026-01-16 04:01:36 +0200

    Create ir_nodes.py

---
+++ ir/ir_nodes.py
@@ -0,0 +1,6 @@
+class IREntity:
+    def __init__(self, name, state, target, increment):
+        self.name = name
+        self.state = state
+        self.target = target
+        self.increment = increment
